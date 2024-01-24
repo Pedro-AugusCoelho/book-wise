f3df4d051363f4ec83b37dab012036ab134401ee
PedroAugusCoelho <peaugusto.coelho@gmail.com>
2024-01-24 20:10:10 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
+import { globalStyles } from '@/styles/global'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+globalStyles()
+
+const nunito = Nunito({ subsets: ['latin'] })
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`${nunito.className}`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,19 @@
+import { globalCss } from '../../stitches.config'
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+  },
+
+  body: {
+    '-webkit-font-smoothing': 'antialiased',
+    backgroundColor: '$gray-800',
+    color: '$white',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: '$default',
+    fontWeight: '$regular',
+  },
+})
